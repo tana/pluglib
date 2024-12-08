c73b698710c33c5100c6a27d45d5f30fe3067fde
Satoshi Tanaka <overthere555@gmail.com>
2024-12-09 04:57:33 +0900

    Implement std::error::Error

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,24 @@ pub enum Error {
     Ble(btleplug::Error),
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            &Error::Protocol(str) => write!(f, "{}", str),
+            &Error::Ble(err) => write!(f, "BLE error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match &self {
+            &Error::Ble(err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
 impl From<btleplug::Error> for Error {
     fn from(value: btleplug::Error) -> Self {
         Error::Ble(value)
